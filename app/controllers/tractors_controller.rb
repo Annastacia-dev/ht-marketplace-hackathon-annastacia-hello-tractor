b8dad8814fd03a2b4c4308a9dccc64edc238a4dd
Annastacia <annetotoh@gmail.com>
2024-11-26 12:23:07 +0300

    Update tractors_controller.rb

--- app/controllers/tractors_controller.rb
+++ app/controllers/tractors_controller.rb
@@ -83,6 +83,6 @@ class TractorsController < ApplicationController
   end
 
   def tractor_params
-    params.require(:tractor).permit([:make, :model, :description, :condition, :year_of_manufacture, :hours_used, :location, :price, :publishing_status, :selling_status, images: []])
+    params.require(:tractor).permit([:make, :model, :description, :condition, :year_of_manufacture, :hours_used, :location, :price, :publishing_status, , images: [], :selling_status])
   end
 end
